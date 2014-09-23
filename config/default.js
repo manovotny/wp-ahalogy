7cce6b00c8edda921c96e51f949fb1d15dc71bae
Michael Novotny <manovotny@gmail.com>
2014-09-23 11:15:03 -0500

    Bumps version for release.

--- config/default.js
+++ config/default.js
@@ -25,7 +25,7 @@ module.exports = (function () {
             slug: 'wp-ahalogy',
             type: 'plugin', // Should be `plugin` or `theme`.
             url: 'https://github.com/manovotny/wp-ahalogy',
-            version: '1.0.0'
+            version: '1.0.1'
         }
     };
 
--- src/classes/class-wp-ahalogy.php
+++ src/classes/class-wp-ahalogy.php
@@ -67,7 +67,7 @@ class WP_Ahalogy {
      *
      * @var string
      */
-    protected $version = '1.0.0';
+    protected $version = '1.0.1';
 
     /* Constructor
     ---------------------------------------------------------------------------------- */
